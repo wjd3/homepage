28e8d8b3a0ef7f7d56a0b28cf54d5e464adb39c0
Liam Davis <liamdavis@tuta.io>
2024-10-18 18:48:54 -0400

    astro compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,14 +5,16 @@ import sitemap from '@astrojs/sitemap'
 import icon from 'astro-icon'
 import vercel from '@astrojs/vercel/static'
 
+import compress from 'astro-compress';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://wjd3.com',
-	integrations: [tailwind(), sitemap(), icon()],
-	output: 'static',
-	adapter: vercel(),
-	image: {
-		domains: ['images.prismic.io'],
-		remotePatterns: [{ protocol: 'https' }]
-	}
+    site: 'https://wjd3.com',
+    integrations: [tailwind(), sitemap(), icon(), compress()],
+    output: 'static',
+    adapter: vercel(),
+    image: {
+        domains: ['images.prismic.io'],
+        remotePatterns: [{ protocol: 'https' }]
+    }
 })
