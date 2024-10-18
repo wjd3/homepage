ffecbec6b390b14a3610ee7b1c0bfeb60cf73292
Liam Davis <liamdavis@tuta.io>
2024-10-18 18:38:10 -0400

    fix sitemaps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import tailwind from '@astrojs/tailwind';
-
-import sitemap from '@astrojs/sitemap';
-
-import icon from 'astro-icon';
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import sitemap from '@astrojs/sitemap'
+import icon from 'astro-icon'
+import vercel from '@astrojs/vercel/static'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), icon()]
-});
+	site: 'https://wjd3.com',
+	integrations: [tailwind(), sitemap(), icon()],
+	output: 'static',
+	adapter: vercel(),
+	image: {
+		domains: ['images.prismic.io'],
+		remotePatterns: [{ protocol: 'https' }]
+	}
+})
